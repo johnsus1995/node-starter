5b9272fb1bf767c97d1cac53d2573fe51b6daa7f
jaison john <jaisonjohny26@gmail.com>
2024-03-07 12:36:37 +0530

    test

--- controllers/auth.controller.ts
+++ controllers/auth.controller.ts
@@ -1,6 +1,6 @@
-import { NextFunction, Request, Response } from "express";
-import { LoginDto, SignupDto } from "../dtos/auth.dto";
-import { AuthService } from "../services/auth.service";
+import { NextFunction, Request, Response } from 'express';
+import { LoginDto, SignupDto } from '../dtos/auth.dto';
+import { AuthService } from '../services/auth.service';
 
 class AuthController {
   public authService = new AuthService();
@@ -10,45 +10,34 @@ class AuthController {
       const userData: SignupDto = req.body;
       const data = await this.authService.signup(userData);
       res.status(200).json({
-        message: "user created",
+        message: 'user created',
         data,
       });
       next();
     } catch (error: any) {
-      res
-        .status(error.status ?? 500)
-        .json({ message: error.message ?? "something went wrong" });
+      res.status(error.status ?? 500).json({ message: error.message ?? 'something went wrong' });
     }
   };
 
   public login = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const userData: LoginDto = req.body;
+
       const data = await this.authService.login(userData);
-      res.status(200).json({message: "Login success",
-        data,
-      });
+      res.status(200).json({ message: 'Login success  ', data });
       next();
     } catch (error: any) {
-      res
-        .status(error.status ?? 500)
-        .json({ message: error.message ?? "something went wrong" });
+      res.status(error.status ?? 500).json({ message: error.message ?? 'something went wrong' });
     }
   };
 
-  public verifyEmail = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
+  public verifyEmail = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const data = await this.authService.verifyEmail(req.params.token);
-      res.status(200).json({message: "Email verified",data,});
+      res.status(200).json({ message: 'Email verified', data });
       next();
     } catch (error: any) {
-      res
-        .status(error.status ?? 500)
-        .json({ message: error.message ?? "something went wrong" });
+      res.status(error.status ?? 500).json({ message: error.message ?? 'something went wrong' });
     }
   };
 }
