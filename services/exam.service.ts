c5faff1db297c45323a9a08102f825378f6ff8a0
johnsus1995 <jaisonjohny26@gmail.com>
2024-06-18 21:08:01 +0530

    exam service

--- services/exam.service.ts
+++ services/exam.service.ts
@@ -1,6 +1,13 @@
+import { Exam } from "../models/Exam";
 
-export class ExamService{
-    public async addExam(){
-        
-    }
+export class ExamService {
+  public async addExam(data: any): Promise<any> {
+    const newExam = await Exam.create({
+      adminId: data.adminId,
+      description: data.description,
+      title: data.title,
+    });
+
+    return newExam;
+  }
 }
